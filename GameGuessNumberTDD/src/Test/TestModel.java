2547d002aff963ee98d34a0c06db2e7d22a2abbe
OlenaStoliarova <stoliarova.elena@gmail.com>
2019-11-24 19:19:53 +0200

    Game GuessNumber TDD: added testSetInitialInterval and code in the Model to make it "green"

--- GameGuessNumberTDD/src/Test/TestModel.java
+++ GameGuessNumberTDD/src/Test/TestModel.java
@@ -1,4 +1,21 @@
 package Test;
 
+import org.junit.Assert;
+import org.junit.Test;
+import ua.training.guessnumber.Model;
+
 public class TestModel {
+    @Test
+    public void testSetInitialInterval(){
+        Model model = new Model();
+
+        //initial interval shouldn't be hardcoded in the Model. here we check that
+        model.setInitialInterval(25, 67);
+        Assert.assertEquals( model.getCurrentGuessRangeMin(), 25);
+        Assert.assertEquals( model.getCurrentGuessRangeMax(), 67);
+
+        model.setInitialInterval(0, 100);
+        Assert.assertEquals( model.getCurrentGuessRangeMin(), 0);
+        Assert.assertEquals( model.getCurrentGuessRangeMax(), 100);
+    }
 }
--- GameGuessNumberTDD/src/ua/training/guessnumber/Model.java
+++ GameGuessNumberTDD/src/ua/training/guessnumber/Model.java
@@ -1,4 +1,19 @@
 package ua.training.guessnumber;
 
 public class Model {
+    private int currentGuessRangeMin;
+    private int currentGuessRangeMax;
+    
+    public void setInitialInterval(int guessRangeMin, int guessRangeMax) {
+        currentGuessRangeMin = guessRangeMin;
+        currentGuessRangeMax = guessRangeMax;
+    }
+
+    public int getCurrentGuessRangeMin() {
+        return currentGuessRangeMin;
+    }
+
+    public int getCurrentGuessRangeMax() {
+        return currentGuessRangeMax;
+    }
 }
