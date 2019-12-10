bc567fcc4312bd5995fa7193b1ca6ac306ffdbb7
OlenaStoliarova <stoliarova.elena@gmail.com>
2019-12-10 16:53:17 +0200

    CreateExceptionTask: teacher's example code will be our base for implementing new task "Create your own exception"

---
+++ CreateExceptionTask/src/main/java/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.controller.Controller;
+import com.company.model.Model;
+import com.company.view.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller(new Model(), new View());
+        // Run
+        controller.processUser();
+    }
+}
