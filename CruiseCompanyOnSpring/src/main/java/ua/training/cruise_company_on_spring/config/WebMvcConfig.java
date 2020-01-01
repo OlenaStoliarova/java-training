1caf7eb123facd75b72306865bd38f719aea7642
OlenaStoliarova <stoliarova.elena@gmail.com>
2020-01-01 16:05:10 +0200

    CruiseCompanyOnSpring: created header.html for easy adding of language choice to any page

--- CruiseCompanyOnSpring/src/main/java/ua/training/cruise_company_on_spring/config/WebMvcConfig.java
+++ CruiseCompanyOnSpring/src/main/java/ua/training/cruise_company_on_spring/config/WebMvcConfig.java
@@ -7,13 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
-@EnableWebMvc
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
