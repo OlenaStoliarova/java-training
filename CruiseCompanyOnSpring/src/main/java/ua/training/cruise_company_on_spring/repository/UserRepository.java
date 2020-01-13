541b4f00a17d859a0ff93c90fd5418aa8c604808
OlenaStoliarova <stoliarova.elena@gmail.com>
2020-01-13 11:29:34 +0200

    CruiseCompanyOnSpring: sometimes it feels like it's easier to just start over

--- CruiseCompanyOnSpring/src/main/java/ua/training/cruise_company_on_spring/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.training.cruise_company_on_spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ua.training.cruise_company_on_spring.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
--- CruiseCompanyOnSpring/src/main/java/ua/training/cruise_company_on_spring/service/NoEntityFoundException.java
+++
@@ -1,7 +0,0 @@
-package ua.training.cruise_company_on_spring.service;
-
-public class NoEntityFoundException extends Exception{
-    public NoEntityFoundException(String message) {
-        super(message);
-    }
-}
